59cc7416b07fca8b050d356b06d101c7c0255e19
Java sauce <mohammedbrandon204@gmail.com>
2022-08-19 21:00:12 -0400

    Create ItemRegistryHandler.java

---
+++ src/main/java/com/devmaster/morecreepsandweirdosreborn/misc/ItemRegistryHandler.java
@@ -0,0 +1,20 @@
+package com.devmaster.mocreepsandweirdosreborn.misc
+
+import com.devmaster.mocreepsandweirdosreborn.misc.MoreCreepsAndWeirdosReborn;
+
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries
+
+public class ItemRegistryHandler{
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MoreCreepsAndWeirdosReborn.MOD_ID);
+
+
+public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
